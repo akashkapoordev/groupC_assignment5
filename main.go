7ce64a9642c14e103472701aef20219e0e835399
bilalnawazdev <bilalnawaz272@gmail.com>
2024-04-03 15:55:54 -0400

    Defined database connection and tables

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+// Define database connection details
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "root"
+	password = "pass123456"
+	dbname   = "users"
+)
 
-}
+// Define database tables
+const (
+	usersTable       = "users"
+	invitationsTable = "invitations"
+	adminsTable      = "admins"
+)
